5fa3807c2aa0cca3159b0d4c27896476affd5c8a
MatiasCS2005 <mcrisosto2024@alu.uct.cl>
2026-04-18 16:37:44 -0400

    Listar solicitudes
    Con estos cambios ahora los trabajadores solo ven sus propias solicitudes que tengan pendientes y los jefes ven todas las solicitudes pendientes.
    
    - En solicitudController.js se cambió el const listarSolicitudes, ahora esta bajo un if que determina según el rol, que solicitudes ve el usuario al iniciar sesión (trabajador: solo las pendientes que él tiene, jefe: todas las solicitudes)

--- controllers/solicitudController.js
+++ controllers/solicitudController.js
@@ -4,25 +4,50 @@ const { renderTemplate } = require('../services/templateService');
 
 exports.listarSolicitudes = async (req, res) => {
   try {
-    const [rows] = await db.query(`
-      SELECT 
-        s.id,
-        s.user_id,
-        s.fecha_inicio,
-        s.fecha_fin,
-        s.estado,
-        s.comentario,
-        s.motivo_rechazo,
-        s.fecha_creacion,
-        s.fecha_resolucion,
-        s.resuelto_por,
-        u.nombre,
-        u.email,
-        u.rol
-      FROM solicitudes s
-      JOIN users u ON s.user_id = u.id
-      ORDER BY s.fecha_creacion DESC
-    `);
+    let rows;
+
+    if (req.user.rol === 'JEFE') {
+      [rows] = await db.query(`
+        SELECT 
+          s.id,
+          s.user_id,
+          s.fecha_inicio,
+          s.fecha_fin,
+          s.estado,
+          s.comentario,
+          s.motivo_rechazo,
+          s.fecha_creacion,
+          s.fecha_resolucion,
+          s.resuelto_por,
+          u.nombre,
+          u.email,
+          u.rol
+        FROM solicitudes s
+        JOIN users u ON s.user_id = u.id
+        ORDER BY s.fecha_creacion DESC
+      `);
+    } else {
+      [rows] = await db.query(`
+        SELECT 
+          s.id,
+          s.user_id,
+          s.fecha_inicio,
+          s.fecha_fin,
+          s.estado,
+          s.comentario,
+          s.motivo_rechazo,
+          s.fecha_creacion,
+          s.fecha_resolucion,
+          s.resuelto_por,
+          u.nombre,
+          u.email,
+          u.rol
+        FROM solicitudes s
+        JOIN users u ON s.user_id = u.id
+        WHERE s.user_id = ?
+        ORDER BY s.fecha_creacion DESC
+      `, [req.user.id]);
+    }
 
     res.json(rows);
   } catch (error) {
